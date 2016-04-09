47516888286ac03d4c9686db3947ab6968aa08b6
Danilo Dorotheu <danilo.dorotheu@live.com>
2016-04-09 15:28:34 -0300

    Criadas as entidades Pessoa e Cliente, sendo a entidade Cliente filha de Pessoa e a entidade Pessoa implementando a interface EntityInterface

---
+++ Cliente.php
@@ -0,0 +1,49 @@
+<?php
+
+class Cliente extends Pessoa
+{
+    private $dtCadastro;
+    private $status;
+
+    /**
+     * Cliente constructor.
+     * @param $data
+     */
+    function __construct($data)
+    {
+        parent::__construct($data);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDtCadastro()
+    {
+        return $this->dtCadastro;
+    }
+
+    /**
+     * @param mixed $dtCadastro
+     */
+    public function setDtCadastro($dtCadastro)
+    {
+        $this->dtCadastro = $dtCadastro;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+}
---
+++ Pessoa.php
@@ -0,0 +1,131 @@
+<?php
+
+class Pessoa implements EntityInterface
+{
+
+    private $id;
+    private $nome;
+    private $sexo;
+    private $cpf;
+    private $rg;
+
+    /**
+     * Pessoa constructor.
+     * @param $data
+     */
+    function __construct($data)
+    {
+        $this->hidrator($data);
+    }
+
+    /**
+     * Transforma o array de dados em um novo objeto
+     *
+     * @param $data
+     */
+    public function hidrator($data)
+    {
+        foreach ($data as $key => $value) {
+            $nomeFuncao = "set" . ucfirst($key);
+            if (method_exists($this, $nomeFuncao) and null !== $value) {
+                $this->{$nomeFuncao}($value);
+            }
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param mixed $nome
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdade()
+    {
+        return $this->idade;
+    }
+
+    /**
+     * @param mixed $idade
+     */
+    public function setIdade($idade)
+    {
+        $this->idade = $idade;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSexo()
+    {
+        return $this->sexo;
+    }
+
+    /**
+     * @param mixed $sexo
+     */
+    public function setSexo($sexo)
+    {
+        $this->sexo = $sexo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCpf()
+    {
+        return $this->cpf;
+    }
+
+    /**
+     * @param mixed $cpf
+     */
+    public function setCpf($cpf)
+    {
+        $this->cpf = $cpf;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRg()
+    {
+        return $this->rg;
+    }
+
+    /**
+     * @param mixed $rg
+     */
+    public function setRg($rg)
+    {
+        $this->rg = $rg;
+    }
+}
