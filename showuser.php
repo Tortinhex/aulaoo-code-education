ca686b6cc108f7cb6a60c3c98e1326590aff58f0
Danilo Dorotheu <danilo.dorotheu@live.com>
2016-04-09 15:43:05 -0300

    - Implementadas melhorias na busca do clientes para listar os seus dados. Agora, se não existir o cliente cadastrado com o ID passado ou se o ID Não for do tipo numerico, exibirá erro na tela

--- showuser.php
+++ showuser.php
@@ -1,39 +1,53 @@
-<?php
-include "layoutHead.php";
-$cliente = $loadCliente->findOneBy("id", $_GET["id"]);
-?>
-    <div class="container">
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                Detalhes do Cliente
+<div class="container">
+    <?php
+    include "layoutHead.php";
+
+    $idCliente = $_GET["id"];
+    $cliente = null;
+    if ($idCliente and is_numeric($idCliente)) {
+        $cliente = $loadCliente->findOneBy("id", $idCliente);
+    }
+
+
+    if (!$cliente) {
+        echo '<div class="alert alert-danger" role="alert">Cliente não encontrado!!</div>';
+        include "layoutFoot.php";
+        die;
+    }
+
+    ?>
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            Detalhes do Cliente
+        </div>
+        <div class="panel-body">
+            <div class="page-header">
+                <a class="btn btn-primary pull-right" href="index.php">Voltar</a>
+                <h1>
+                    <small>Cliente</small><?php echo $cliente->getNome() ?></h1>
             </div>
-            <div class="panel-body">
-                <div class="page-header">
-                    <a class="btn btn-primary pull-right" href="index.php">Voltar</a>
-                    <h1><small>Cliente </small><?php echo $cliente->getNome() ?></h1>
-                </div>
-                <div class="row">
-                    <div class="col-md-2"><p>CPF:</p></div>
-                    <div class="col-md-10"><p class="lead"><?php echo $cliente->getCpf() ?></p></div>
-                </div>
-                <div class="row">
-                    <div class="col-md-2"><p>RG:</p></div>
-                    <div class="col-md-10"><p class="lead"><?php echo $cliente->getRg() ?></p></div>
-                </div>
-                <div class="row">
-                    <div class="col-md-2"><p>Sexo:</p></div>
-                    <div class="col-md-10"><p class="lead"><?php echo $cliente->getSexo() ?></p></div>
-                </div>
-                <div class="row">
-                    <div class="col-md-2"><p>Data de Cadastro:</p></div>
-                    <div class="col-md-10"><p class="lead"><?php echo $cliente->getDtCadastro() ?></p></div>
-                </div>
-                <div class="row">
-                    <div class="col-md-2"><p>Status:</p></div>
-                    <div class="col-md-10"><p class="lead"><?php echo $cliente->getStatus() ?></p></div>
-                </div>
+            <div class="row">
+                <div class="col-md-2"><p>CPF:</p></div>
+                <div class="col-md-10"><p class="lead"><?php echo $cliente->getCpf() ?></p></div>
+            </div>
+            <div class="row">
+                <div class="col-md-2"><p>RG:</p></div>
+                <div class="col-md-10"><p class="lead"><?php echo $cliente->getRg() ?></p></div>
+            </div>
+            <div class="row">
+                <div class="col-md-2"><p>Sexo:</p></div>
+                <div class="col-md-10"><p class="lead"><?php echo $cliente->getSexo() ?></p></div>
+            </div>
+            <div class="row">
+                <div class="col-md-2"><p>Data de Cadastro:</p></div>
+                <div class="col-md-10"><p class="lead"><?php echo $cliente->getDtCadastro() ?></p></div>
+            </div>
+            <div class="row">
+                <div class="col-md-2"><p>Status:</p></div>
+                <div class="col-md-10"><p class="lead"><?php echo $cliente->getStatus() ?></p></div>
             </div>
         </div>
     </div>
+</div>
 
 <?php include "layoutFoot.php" ?>
