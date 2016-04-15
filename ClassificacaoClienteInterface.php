7607266d62279b7f0868b071bd39bc0f7d98a9c7
Danilo Dorotheu <danilo.dorotheu@live.com>
2016-04-14 22:24:57 -0300

    - Inclusao da interface ClassificacaoClienteInterface para obrigar as entidades que irão utilizá-la, implementar a funcao de classificacao do cliente;

---
+++ ClassificacaoClienteInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: danilo
+ * Date: 14/04/16
+ * Time: 22:22
+ */
+interface ClassificacaoClienteInterface
+{
+    public function setClassificacao($classificacao);
+    public function getClassificacao();
+}
