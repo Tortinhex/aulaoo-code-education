5d20993a68a818e0463ba87cd1ee237b7c5b9a5d
Danilo Dorotheu <danilo.dorotheu@live.com>
2016-04-14 21:49:32 -0300

    - Inserida a interface do tipo de cliente, para obrigar os clientes a implementar o endereco de cobranca;

---
+++ TipoClienteInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: danilo
+ * Date: 14/04/16
+ * Time: 21:47
+ */
+interface TipoClienteInterface
+{
+    public function setEnderecoCobranca($endereco);
+    public function getEnderecoCobranca();
+}
